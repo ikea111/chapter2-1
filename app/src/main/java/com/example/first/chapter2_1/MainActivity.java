f6256b3494320e73f80c22d71658162097c089f5
ikea111 <1072497122@qq.com>
2018-05-12 21:47:59 +0800

    update

--- app/src/main/java/com/example/first/chapter2_1/MainActivity.java
+++ app/src/main/java/com/example/first/chapter2_1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.first.chapter2_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button linearbtn = (Button)  findViewById(R.id.linearbtn);
+        linearbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, activity_linear .class );
+                startActivity(intent);
+
+            }
+        });
+        Button relativebtn = (Button)  findViewById(R.id.relativebtn);
+        relativebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, activity_relative .class );
+                startActivity(intent);
+
+            }
+        });
+        Button framebtn = (Button)  findViewById(R.id.framebtn);
+        framebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, activity_frame .class );
+                startActivity(intent);
+
+            }
+        });
+        Button tablebtn = (Button)  findViewById(R.id.tablebtn);
+        tablebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, activity_table .class );
+                startActivity(intent);
+
+            }
+        });
+        Button gridbtn = (Button)  findViewById(R.id.gridbtn);
+        gridbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, activity_grib .class );
+                startActivity(intent);
+
+            }
+        });
     }
 }
